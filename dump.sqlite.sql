e2c7d139b3d93a26b615b49d3bc131d08e93cbcf
pxiss <159054757+pxiss@users.noreply.github.com>
2024-08-12 13:52:57 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE CAMPO(
+  ID INT(4),
+  CIDADE VARCHAR(30)UNIQUE,
+  CAPACIDADE INT(4)NOT NULL
+  
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE jogador(
+  id INT(4),
+  NOME VARCHAR(30),
+  ESPORTE VARCHAR(30)
+  
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
